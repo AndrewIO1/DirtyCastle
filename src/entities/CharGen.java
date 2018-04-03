caa06b399d65436639be1d56ea1ba7d8686f2564
Ravingrabb <dog1995@alumni.nottingham.ac.uk>
2018-04-03 23:22:05 +0500

    Ээээй, долбаёёёёб!
    генератор 0.3.1

--- src/entities/CharGen.java
+++ src/entities/CharGen.java
@@ -8,33 +8,39 @@ public class CharGen {
 	 * @param args
 	 * @return 
 	 */
-//	..методы
+//	генератор СЭКСА
 	private static void sexGen(TestCreature.Builder citizen) {
 		//генерация пола
 		int sex =  1 + (int)(Math.random() * 2);
 		citizen.sex(sex);
 	}
 	
-	private static String[] maleFirstSyll = {"Vladi","Kosti","Brone","Kila", "Andre", "Meta", "Stan"};
-	private static String[] maleSecondSyll = {"slav","mir","don","var","gon"};
-	private static String[] fmaleFirstSyll = {"Stel", "Skal", "Flo", "Ka", "Si"};
-	private static String[] fmaleSecondSyll = {"mi", "la", "li", "den"};
-	private static String[] fmaleThirdSyll = {"cia", "na", "nia"};
+//	генератор имени
+// заполняем массивы слогами
+	private static String[] male1s = {"Vladi","Kosti","Brone","Kila", "Andre", "Meta", "Stan"};
+	private static String[] male2s = {"slav","mir","var","tard"};
+	private static String[] fmale1s = {"Stel", "Skal", "Flo", "Ka", "Si"};
+	private static String[] fmale2s = {"mi", "la", "li", "den"};
+	private static String[] fmale3s = {"cia", "na", "nia"};
 
 	private static void nameGen(TestCreature.Builder citizen) {
 		String name;
 		if (citizen.sex() == 1) {
-			name = maleFirstSyll[DwarfsGame.rnd.nextInt(maleFirstSyll.length)] + maleSecondSyll[DwarfsGame.rnd.nextInt(maleSecondSyll.length)];
+			String rand1 = male1s[DwarfsGame.rnd.nextInt(male1s.length)];
+			String rand2 = male2s[DwarfsGame.rnd.nextInt(male2s.length)];
+			name = rand1 + rand2;
 		}
 		else {
-			name = fmaleFirstSyll[DwarfsGame.rnd.nextInt(fmaleFirstSyll.length)] + fmaleSecondSyll[DwarfsGame.rnd.nextInt(fmaleSecondSyll.length)] + fmaleThirdSyll[DwarfsGame.rnd.nextInt(fmaleThirdSyll.length)];
+			String rand1 = fmale1s[DwarfsGame.rnd.nextInt(fmale1s.length)];
+			String rand2 = fmale2s[DwarfsGame.rnd.nextInt(fmale2s.length)];
+			String rand3 = fmale3s[DwarfsGame.rnd.nextInt(fmale3s.length)];
+			name = rand1 + rand2 + rand3;
 			}
-		citizen.name(name) ;
-		
+		citizen.name(name) ;	
 		
 	}
 	
-
+// генератор основных статов
 	private static void mainStatGen(TestCreature.Builder citizen) {
 		citizen.maxHp(100);
 		citizen.speed(0.1f);
@@ -44,6 +50,8 @@ public class CharGen {
 		citizen.mood(50); //настроение
 	}
 	
+	
+//	генератор типа тела
 	private static void bodyGen(TestCreature.Builder citizen) {
 		int[] bodyStats = new int[3]; //статы тела
 		//определение телосложения по Шелдону
@@ -86,8 +94,7 @@ public class CharGen {
 	
 
 
-	//НОВЫЙ ГЕНЕРАТОР ВЫШЕ, ПОКА ТЕСТ
-	//genirator 0.3
+	//genirator 0.3.1
 	public static TestCreature.Builder charGen(TestCreature.Builder citizen){
 		sexGen(citizen);
 		nameGen(citizen);
